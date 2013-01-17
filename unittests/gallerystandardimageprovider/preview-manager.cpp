ba66e147ed8949856cd04c8812a12ced44e0172b
Nicolas d'Offay <nicolas.doffay@canonical.com>
2013-01-17 15:07:17 +0000

    Removed comment.

--- unittests/gallerystandardimageprovider/preview-manager.cpp
+++ unittests/gallerystandardimageprovider/preview-manager.cpp
@@ -46,7 +46,6 @@ void PreviewManager::on_media_data_altered()
 
 }
 
-//Get thumbnail dir.
 QFileInfo PreviewManager::ThumbnailFileFor(const QFileInfo &file) const
 {
     return QFileInfo(file.dir(), PREVIEW_DIR + "/" + file.completeBaseName() + "_th_s." + PREVIEW_FILE_EXT);
