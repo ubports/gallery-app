764674e1a56ce1daf6883faeb756c7785f3bba8a
Guenter Schwann <guenter.schwann@canonical.com>
2013-02-22 14:29:39 +0100

    Make test usable, even ia an older test didn't clean up the test data dir

--- tests/autopilot/gallery/tests/__init__.py
+++ tests/autopilot/gallery/tests/__init__.py
@@ -27,14 +27,15 @@ class GalleryTestCase(AutopilotTestCase, QtIntrospectionTestMixin):
     """A common test case class that provides several useful methods for gallery tests."""
 
     sample_dir = "/tmp/ubuntu-gallery_ap"
-    sample_file = "/tmp/ubuntu-gallery_ap/sample.jpg"
+    sample_file = sample_dir + "/sample.jpg"
     installed_sample_file = "/usr/lib/python2.7/dist-packages/ubuntu-gallery/data/sample.jpg"
     local_sample_file = "gallery/data/sample.jpg"
 
     def setUp(self):
         super(GalleryTestCase, self).setUp()
 
-        os.makedirs(self.sample_dir)
+        if (not os.path.exists(self.sample_dir)):
+            os.makedirs(self.sample_dir)
         self.assertTrue(os.path.exists(self.sample_dir))
         # Lets assume we are installed system wide if this file is somewhere in /usr
         if os.path.realpath(__file__).startswith("/usr/"):
