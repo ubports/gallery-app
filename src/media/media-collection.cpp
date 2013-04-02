26baf5e256d7da892624d08c6a8af1794fe17948
Guenter Schwann <guenter.schwann@canonical.com>
2013-04-02 14:04:45 +0200

    Code cleanups

--- src/media/media-collection.cpp
+++ src/media/media-collection.cpp
@@ -17,7 +17,6 @@
  * Jim Nelson <jim@yorba.org>
  */
 
-#include <QApplication>
 #include <QFileInfo>
 #include <QStringList>
 #include <QString>
@@ -45,11 +44,6 @@ MediaCollection::MediaCollection(const QDir& directory)
     QStringList filenames = directory_.entryList();
     QString filename;
     foreach (filename, filenames) {
-        // stat'ing and sync'ing file info over even several hundred photos is an
-        // expensive operation since it involves lots of I/O, so spin the event
-        // loop so that the UI remains responsive
-        QApplication::processEvents();
-
         QFileInfo file(directory_, filename);
 
         Photo *p = Photo::Load(file);
