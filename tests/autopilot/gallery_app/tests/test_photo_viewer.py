9925614792afc69a28643ad08d111265910a73e8
Christopher Lee <chris.lee@canonical.com>
2013-10-30 18:01:48 +1300

    Initial updates for AP 1.4

--- tests/autopilot/gallery_app/tests/test_photo_viewer.py
+++ tests/autopilot/gallery_app/tests/test_photo_viewer.py
@@ -60,16 +60,18 @@ class TestPhotoViewer(TestPhotoViewerBase):
         super(TestPhotoViewer, self).setUp()
 
     def get_delete_dialog(self):
-        self.assertThat(lambda: self.photo_viewer.get_delete_dialog(),
-                        Eventually(Not(Is(None))))
         delete_dialog = self.photo_viewer.get_delete_dialog()
         self.assertThat(delete_dialog.visible, Eventually(Equals(True)))
         self.assertThat(delete_dialog.opacity, Eventually(Equals(1)))
         return delete_dialog
 
     def ensure_closed_delete_dialog(self):
-        self.assertThat(lambda: self.photo_viewer.get_delete_dialog(),
-                        Eventually(Is(None)))
+        self.assertThat(self.photo_viewer.delete_dialog_shown,
+                        Eventually(Is(False)))
+        # self.assertThat(
+        #     lambda: self.photo_viewer.get_delete_dialog(wait=False),
+        #     Eventually(Is(None))
+        # )
 
     def test_nav_bar_back_button(self):
         """Clicking the back button must close the photo."""
