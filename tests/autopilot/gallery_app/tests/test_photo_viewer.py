36bf42da26b47b97d43aa08e94d5677f2d2e4ad5
Ugo Riboni <ugo.riboni@canonical.com>
2014-02-05 12:15:13 +0100

    Fix and improve the unit test

--- tests/autopilot/gallery_app/tests/test_photo_viewer.py
+++ tests/autopilot/gallery_app/tests/test_photo_viewer.py
@@ -168,29 +168,22 @@ class TestPhotoViewer(TestPhotoViewerBase):
         self.assertThat(list.currentIndex, Eventually(Equals(0)))
 
         # Slide left should move to the next image
-        x, y, h, w = list.globalRect
-        self.pointing_device.move(x + w - 10, y + h / 2)
-        self.pointing_device.press()
-        self.pointing_device.move(x + 10, y + h / 2)
-        self.pointing_device.release()
+        x, y, w, h = list.globalRect
+        mid_y = y + h / 2
+        mid_x = x + w / 2
+        self.pointing_device.drag(mid_x, mid_y, x + 10, mid_y)
 
         self.assertThat(list.moving, Eventually(Equals(False)))
         self.assertThat(list.currentIndex, Eventually(Equals(1)))
 
         # Slide right should get us back to the start
-        self.pointing_device.move(x + 10, y + h / 2)
-        self.pointing_device.press()
-        self.pointing_device.move(x + w - 10, y + h / 2)
-        self.pointing_device.release()
+        self.pointing_device.drag(mid_x, mid_y, x + w - 10, mid_y)
 
         self.assertThat(list.moving, Eventually(Equals(False)))
         self.assertThat(list.currentIndex, Eventually(Equals(0)))
 
         # Slide right again shouldn't go anywhere
-        self.pointing_device.move(x + 10, y + h / 2)
-        self.pointing_device.press()
-        self.pointing_device.move(x + w - 10, y + h / 2)
-        self.pointing_device.release()
+        self.pointing_device.drag(mid_x, mid_y, x + w - 10, mid_y)
 
         self.assertThat(list.moving, Eventually(Equals(False)))
         self.assertThat(list.currentIndex, Eventually(Equals(0)))
